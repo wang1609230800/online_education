56de9530ebc1d3f5da459d902132ed5797dbbc40
wangyijian <1609230800@qq.com>
2024-06-19 10:03:18 +0800

    初始化项目

---
+++ src/main/java/cn/online_education/online_education/OnlineEducationApplication.java
@@ -0,0 +1,13 @@
+package cn.online_education.online_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineEducationApplication.class, args);
+    }
+
+}
